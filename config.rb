a0a511ae05328a8cfe184860bec40088ed925df8
Jeremy Caldwell <eternalistic@72964.no-reply.drupal.org>
2012-11-01 14:23:59 -0700

    Updated styling and layout so it's simpler and easier to modify as a base.

--- config.rb
+++ config.rb
@@ -1,48 +1,22 @@
-#
-# This file is only needed for Compass/Sass integration. If you are not using
-# Compass, you may safely ignore or delete this file.
-#
-# If you'd like to learn more about Sass and Compass, see the sass/README.txt
-# file for more information.
-#
+##
+## This file is intentionally not in the manifest.rb or the gemspec.
+##
+## Before committing any changes to the unit tests, run compass compile with
+## this config.rb file so that the compiled CSS can be added to the repository.
+##
 
-
-# Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-#environment = :production
-
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = false
-#firesass = true
-
-
-# Location of the theme's resources.
-css_dir         = "css"
-sass_dir        = "sass"
-extensions_dir  = "sass-extensions"
-images_dir      = "images"
-javascripts_dir = "js"
-
-
-# Require any additional compass plugins installed on your system.
-#require 'ninesixty'
 require 'zen-grids'
 
 
-##
-## You probably don't need to edit anything below this.
-##
+sass_dir = "sass"
+css_dir  = "css"
 
-# You can select your preferred output style here (can be overridden via the command line):
+extensions_dir = "sass-extensions"
+images_dir = "images"
+javascripts_dir = "js"
+
+environment = :development
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
-
-# To enable relative paths to assets via compass helper functions
+output_style = :expanded
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
-
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+sass_options = {}
