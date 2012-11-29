2f409e40eabd40d359f319873654ea4d57ced7db
Jeremy Caldwell <jcaldwell@magnersanborn.com>
2012-11-29 09:42:11 -0800

    Updated script and coding for grid overlay

--- js/grid-overlay.js
+++ js/grid-overlay.js
@@ -1,17 +1,7 @@
-// Grid overlay
-function toggle_visibility(id) {
-	var e = document.getElementById(id);
-	if(e.style.display == 'block')
-    e.style.display = 'none';
-	else
-    e.style.display = 'block';
-}
-
-
 // Grid overlay toggle
 $(document).ready(function () {
-	$('#grid-toggle a').click(function() {
+	$('a#gridoverlay').click(function() {
     $(this).text($(this).text() == 'Enable' ? 'Disable' : 'Enable');
-    return false;
+    $('#grid-overlay').toggle();
 	})
 });
